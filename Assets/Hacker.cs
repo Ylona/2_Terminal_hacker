3f361c006c616fa618fb47c1f43fa496d15253ba
ylonaMeeuwenberg <meeuwenberg.ylona@gmail.com>
2021-01-29 17:28:26 +0100

    Initial commit

---
+++ Assets/Hacker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    int level;
+    Screen currentScreen;
+    enum Screen { MainMenu, Password, Win };
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input.ToLower() == "menu")
+        {
+            ShowMainMenu();
+        } else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainManu(input);
+        }
+    }
+
+    void RunMainManu(string input)
+    {
+        if (input == "1" || input == "2")
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level!");
+        }
+    }
+
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2_Terminal_hacker
+ Terminal hacker from Complete C# Unity Game Developer 3D
